1bfa75766c46ce812ecf2879cd2194b985a40a55
Roberto Martinez <romartinezg.w@gmail.com>
2023-09-02 13:56:30 -0400

    commit general

--- consultas.js
+++ consultas.js
@@ -1,24 +1,74 @@
+require('dotenv').config();
 const { Pool } = require('pg')
+
 const pool = new Pool({
     host: 'localhost',
-    user: 'postgres',
-    password: 'minirob28',
-    database: 'likeme',
-    port: '5432',
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
     allowExitOnIdle: true
 })
 
 const agregarPost = async (titulo, url, descripcion) => {
-    const consulta = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3)"
+    
+    try {
+    const consulta = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3, 0)"
     const values = [titulo, url, descripcion]
     const result = await pool.query(consulta, values)
     console.log("Post agregado")
+    } catch (error) {
+        throw { code: error.code, message: error.message}
+    }
 }
 
 const getPosts = async () => {
+    try {
     const { rows } = await pool.query("SELECT * from posts");
     console.log(rows);
     return rows;
+    } catch (error) {
+        throw { code: error.code, message: error.message}
+    }
 }
 
-module.exports = { agregarPost, getPosts }
+//////////////
+
+const getPost = async (id) => {
+    try {
+      const consulta = 'SELECT * FROM posts WHERE id = $1';
+      const { rows } = await pool.query(consulta, [id]);
+      console.log(`
+        Registro  id ${id} obtenido correctamente
+          `);
+      return rows;
+    } catch (error) {
+      throw { code: error.code, message: error.message };
+    }
+  };
+  
+  const like = async (id) => {
+    try {
+      const consulta = 'UPDATE posts SET likes = likes + 1 WHERE id = $1';
+      await pool.query(consulta, [id]);
+      console.log(`
+    Se ha agrego un like a id ${id}
+            `);
+    } catch (error) {
+      throw { code: error.code, message: error.message };
+    }
+  };
+  
+  const eliminarPost = async (id) => {
+    try {
+      const consulta = 'DELETE FROM posts WHERE id = $1';
+      await pool.query(consulta, [id]);
+      console.log(`
+        Se ha eliminado correctamente el registro id ${id}
+                `);
+    } catch (error) {
+      throw { code: error.code, message: error.message };
+    }
+  };
+  
+module.exports = { agregarPost, getPosts, getPost, like, eliminarPost }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { agregarPost, getPosts } = require('./consultas')
+const { agregarPost, getPosts, getPost, like, eliminarPost} = require('./consultas')
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,8 +6,9 @@ var bodyParser = require('body-parser')
 
 app.use(express.json())
 
-app.listen(3000, console.log('Servidor encendido en el Puerto 3000'))
+app.disable('x-powered-by');
 
+app.listen(3000, console.log('Servidor encendido en el Puerto 3000'))
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -18,14 +19,68 @@ app.use(
 );
 
 app.get("/posts", async (req, res) => {
+    try {
     const posts = await getPosts()
     res.json(posts)
+    } catch(error) {
+      console.error(`
+      Se produjo un error -> Error (${error.code}): ${error.message}
+    `);
+    res.status(500).send('Error en Servidor');
+  }
 })
 
 app.post("/posts", async (req, res) => {
+    try {
     const { titulo, url, descripcion } = req.body
     const resultado = await agregarPost(titulo, url, descripcion)
     res.json(resultado)
     console.log('Post agregado correctamente')
-    res.send('Post agregado con exito')
+    } catch(error) {
+      console.error(`Se produjo un error -> Error (${error.code}): ${error.message}
+      `);
+      res.status(500).send('Error en Servidor');
+    }
 })
+
+/////////////////////////////////////////////////////////////
+
+app.get('/posts/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const resultado = await getPost(id);
+    res.json(resultado);
+  } catch (error) {
+    console.error(`
+     Se produjo un error -> Error (${error.code}): ${error.message}
+    `);
+    res.status(500).send('Error en Servidor');
+  }
+});
+
+
+app.put('/posts/like/:id', async (req, res) => {
+    const { id } = req.params;
+  try {
+    await like(id);
+    res.send(`Se agrego un like`);
+  } catch (error) {
+    console.error(`
+    Se produjo un error -> Error (${error.code}): ${error.message}
+    `);
+    res.status(500).send('Error en Servidor');
+  }
+});
+
+app.delete('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await eliminarPost(id);
+    return res.send('Se elimino post');
+  } catch (error) {
+    console.error(`
+    Se produjo un error -> Error (${error.code}): ${error.message}
+    `);
+    res.status(500).send('Error en Servidor');
+  }
+});
